464a98257dcee24bc35eb285039250133e56f1de
BlueBug12 <benson.liu.himself@gmail.com>
2021-11-19 14:36:16 +0800

    fixed scale declining bug

--- prototype.py
+++ prototype.py
@@ -8,6 +8,17 @@ from datetime import datetime
 class SA:
 
     def __init__(self,descent_rate: float, initial_t: float, final_t: float, scale: float, x_min, x_max, markov_iter: int, n_var: int):
+        self.n_var = n_var
+        self.x_min = np.zeros(n_var)
+        self.x_max = np.zeros(n_var)
+        self.state = np.zeros(n_var)
+        self.solution = np.zeros(n_var)
+        self.energy = 0
+        self.e_now_record = []
+        self.e_best_record = []
+        self.iter_record = []
+        randseed = random.randint(1,100)
+        random.seed(randseed)
         self.setParam(descent_rate,initial_t,final_t,scale, x_min,x_max,markov_iter,n_var)
 
     def setParam(self, descent_rate: float, initial_t: float, final_t: float, scale:float, x_min, x_max, markov_iter: int, n_var: int):
@@ -15,23 +26,11 @@ class SA:
         self.initial_t = initial_t
         self.final_t = final_t
         self.scale = scale
-
-        self.x_min = np.zeros((n_var))
-        self.x_max = np.zeros((n_var))
-
         self.x_min[:] = x_min[:]
         self.x_max[:] = x_max[:]
-        self.n_var = 0
-        self.energy = 0
         self.markov_iter = markov_iter
-        self.e_now_record = []
-        self.e_best_record = []
-        self.solution =[]
-        self.iter_record = []
     
     def setInitialState(self, state):
-        self.n_var = len(state)
-        self.state = np.zeros(self.n_var)
         self.state[:] = state[:]
         self.energy = self.getEnergy(state)
 
@@ -39,8 +38,6 @@ class SA:
         return math.exp(-delta_energy/temperature)
     
     def run(self):
-        #randseed = random.randint(1,100)
-        #random.seed(randseed)
         accept_good = 0
         accept_bad = 0
         reject_bad = 0
@@ -51,19 +48,19 @@ class SA:
         cur_state[:] = self.state[:]
 
         new_state = np.zeros(self.n_var)
- 
+
         cur_e = self.getEnergy(cur_state)
         best_e = cur_e
         while cur_t >= self.final_t:
             for k in range(self.markov_iter):
                 new_state[:] = self.neighbor(cur_state)
+
                 new_e = self.getEnergy(new_state)
                 delta_e = new_e - cur_e
                 if delta_e < 0:
                     cur_e = new_e
                     cur_state[:] = new_state[:]
                     accept_good += 1
-                    self.scale = self.scale*0.99
                 else:
                     prob = self.acceptance(delta_e, cur_t)
                     if prob > random.random():
@@ -75,6 +72,7 @@ class SA:
                 if(best_e > cur_e):
                     best_e = cur_e
                     self.solution[:] = cur_state[:]
+                    self.scale = self.scale*0.99
             self.e_best_record.append(round(best_e,4))
             self.e_now_record.append(round(cur_e,4))
             self.iter_record.append(self.num_iter)
@@ -91,12 +89,12 @@ class SA:
 
 
     def neighbor(self, state):
-        new_state = np.zeros(self.n_var)
-        new_state[:] = state[:]
+        _new_state = np.zeros(self.n_var)
+        _new_state[:] = state[:]
         v = random.randint(0,len(state)-1)
-        new_state[v] = state[v] + self.scale*(self.x_max[v]-self.x_min[v])*random.normalvariate(0,1) 
-        new_state[v] = max(min(new_state[v],self.x_max[v]),self.x_min[v])
-        return new_state
+        _new_state[v] = state[v] + self.scale*(self.x_max[v]-self.x_min[v])*random.normalvariate(0,1) 
+        _new_state[v] = max(min(_new_state[v],self.x_max[v]),self.x_min[v])
+        return _new_state
 
     def getEnergy(self,state):
         sum_ = 0.0
@@ -118,7 +116,15 @@ class SA:
 def main():
     x_min = [-500,-500]
     x_max = [500,500]
-    a = SA(0.98,100.0,1,0.5,x_min,x_max,100,2)
+    a = SA(
+            descent_rate = 0.98,
+            initial_t = 100.0,
+            final_t = 1,
+            scale = 0.5,
+            x_min = x_min,
+            x_max = x_max,
+            markov_iter = 100,
+            n_var = 2)
     x_initial = np.zeros(2)
     for v in range(2):
         x_initial[v] = random.uniform(x_min[v],x_max[v])
